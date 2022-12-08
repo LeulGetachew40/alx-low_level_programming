73015944a1609193b9e0ca3ed08527d61ab8b793
Leul Getachew <leulgetachew40@gmail.com>
2022-12-08 09:43:54 -0800

    mandatory 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - This program's description
+ * Return - Always 0 (Success)
+**/
+int main(0)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
