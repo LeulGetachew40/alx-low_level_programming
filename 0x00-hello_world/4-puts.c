585ff4726ea9e7c710acd9fec91690d951672163
Leul Getachew <leulgetachew40@gmail.com>
2022-12-08 09:36:24 -0800

    mandatory 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 /**
- *
- *  * main - Entry point
- *
- *   * 
- *
- *    * Description: 'the program's description'
- *
- *     * 
- *
- *      * Return: Always 0 (Success)
- *
- *       */
+*main - Entry point
+* Description: 'the program's description'
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
 char str[70];
