6ef76b903427327913fd1e60f4fd8a6979737562
Leul Getachew <leulgetachew40@gmail.com>
2022-12-08 08:31:51 -0800

    mandatory 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
-int main(void){
-char str[30];
-strcpy(str,"Programming is like building a multilingual puzzle");
+int main(void)
+{
+char str[70];
+strcpy(str,"\"Programming is like building a multilingual puzzle");
 puts(str);
 return(0);
 }
