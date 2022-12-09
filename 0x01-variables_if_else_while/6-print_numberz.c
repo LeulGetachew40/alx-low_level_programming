bbaadd4fedfc3ce9df66a619d7dc5b6a53d7a6de
Leul Getachew <leulgetachew40@gmail.com>
2022-12-09 13:32:55 -0800

    mandatory 7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*Description - This program's description
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+	putchar(i + '0');
+putchar('\n');
+return (0);
+}
