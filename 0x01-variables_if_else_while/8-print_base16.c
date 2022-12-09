d234dd3188cec49440bd597e4447a657213175cd
Leul Getachew <leulgetachew40@gmail.com>
2022-12-09 14:24:20 -0800

    manatory 9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*Description - This program's description
+*/
+int main(void)
+{
+int i;
+char letter;
+for (i = 0; i < 10; i++)
+	putchar(i+'0');
+for (letter = 'a'; letter <= 'f'; letter++)
+	putchar(letter);
+putchar('\n');
+return (0);
+}
