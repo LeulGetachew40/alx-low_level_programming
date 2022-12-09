ee00ae5dfb5d304b42e57b0137307588f827b838
Leul Getachew <leulgetachew40@gmail.com>
2022-12-09 11:08:32 -0800

    mandatory 2

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*Description - This program's description
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("Last digit of %d is ", n);
+if (n < 5)
+}
+printf("and is greater than 5\n");
+}
+else if (n == 0)
+}
+printf("and is 0\n");
+}
+else if (n < 6 && n != 0)
+{
+printf("and is less than 6 and not 0\n");
+}
+return (0);
+}
