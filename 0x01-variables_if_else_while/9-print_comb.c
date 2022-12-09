85c14010d929ceb4345585fc66b2784828884eab
Leul Getachew <leulgetachew40@gmail.com>
2022-12-09 15:06:09 -0800

    mandatory 10

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*Description - This program's description
+*/
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+	putchar((i % 10) + '0');
+	if (i == 9)
+	{
+		continue;
+	}
+	putchar(',');
+	putchar(' ');
+}
+putchar('\n');
+return (0);
+}
