dd17b9185aafadd67626a37d8c6fab4d5ad0fd6b
Leul Getachew <leulgetachew40@gmail.com>
2022-12-13 14:24:51 -0800

    mandatory 11

--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -1,10 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 /**
 *main - check the code
 *Return: Always 0.
 */
 int main(void)
 {
-times_table();
+int n;
+n = add(89, 9);
+printf("%d\n", n);
 return (0);
 }
