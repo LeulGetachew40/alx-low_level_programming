994cf478123eef1f552d927c5a79843e90e23af2
Leul Getachew <leulgetachew40@gmail.com>
2022-12-13 09:29:58 -0800

    mandatory 7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/*
+*_abs - returns the absolute value of the number passed.
+*@n: the parameter passed
+*Return - Always 0.
+*/
+int _abs(int n)
+{
+if (n < 0)
+{
+return (n *= -1);
+}
+else if (n == 0)
+{
+return (0);
+}
+else
+{
+return (n);
+}
+}
