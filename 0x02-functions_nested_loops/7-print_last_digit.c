88672811be6e1bb6a70057b0391d9605ea18dd97
Leul Getachew <leulgetachew40@gmail.com>
2022-12-13 11:11:57 -0800

    mandatory 8

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*print_last_digit - prints the last digit of numbers.
+*@n: the parameter passed
+*Return: Always 0 (Success)
+*/
+int print_last_digit(int n)
+{
+return (n%10);
+}
