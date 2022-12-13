d44809f224015d0d78e67a5001c61ae7c335d612
Leul Getachew <leulgetachew40@gmail.com>
2022-12-13 07:50:21 -0800

    mandatory 5

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,9 @@
 #include "main.h"
-/*
-*_isalpha - determines whether the input is an alphabet or not
+/**
+*_isalpha - determines whether the input is an alphabet or not.
 *@c: a paramter
 *Return: Always 0 (Success)
-*/
+**/
 int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
