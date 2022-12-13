29b18676062d05e4d23c233026735096c5bd8ea5
Leul Getachew <leulgetachew40@gmail.com>
2022-12-13 05:26:06 -0800

    mandatory 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "_putchar.c"
+/**
+* main - check the code
+* Return: Always 0.
+*/
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+return (0);
+}
