4808d74906d9d5fa8b266dfd51b8597cfa0445b0
Leul Getachew <leulgetachew40@gmail.com>
2022-12-15 07:54:54 -0800

    mandatory 2

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 /**
 *_isdigit - checks whether the input is a number or not.
 *@c: the parameter to be checked.
-*Return: Always 0.
+*Return: 1 if the number is a digit, 0 otherwise.
 */
 int _isdigit(int c)
 {
