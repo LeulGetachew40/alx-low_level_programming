2a7af86a170e8d74917791758a4a4eae9a8da1fb
Leul Getachew <leulgetachew40@gmail.com>
2022-12-22 09:27:41 -0800

    mandatory 1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
-*strcat - concatenates the string pointed by @src.
+*_strcat - concatenates the string pointed by @src.
 *@dest: a pointer to the string to be concatenated upon.
 *@src: the source string to be appended.
 *Return: A pointer to the destination string.
@@ -8,13 +9,16 @@
 char *_strcat(char *dest, char *src)
 {
 int index = 0, dest_len = 0;
+
 while (dest[index++])
 {
 dest_len++;
 }
+
 for (index = 0; src[index]; index++)
 {
 dest[dest_len++] = src[index];
 }
+
 return (dest);
 }
