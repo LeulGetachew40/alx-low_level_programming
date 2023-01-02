8579d9b7dae36ca7f14e8309e9d0aa36079f1b2c
Leul Getachew <leulgetachew40@gmail.com>
2023-01-02 06:37:16 -0800

    mandatory 1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _memset: fills the first n bytes of the memory area pointed by s.
  *@s: pointer block of memory to fill
@@ -9,9 +10,11 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
+
 for(i = 0; i < n; i++)
 {
 *s[i] = b;
 }
-rerurn (s);
+
+return (s);
 }
