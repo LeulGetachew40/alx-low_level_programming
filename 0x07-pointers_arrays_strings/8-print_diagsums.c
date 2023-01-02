bd529f6486034778d368c956168e8af46fa09b56
Leul Getachew <leulgetachew40@gmail.com>
2023-01-02 10:43:15 -0800

    mandatory 9

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,10 +17,10 @@ sum1 = 0;
 sum2 = 0;
 
 i = 0;
-while(i < size)
+while (i < size)
 {
-sum1 = sum1 + *(a + i * size + i);
-sum2 = sum2 + *(a + i * size + size - i - 1);
+sum1 = sum1 + *(a + i *size + i);
+sum2 = sum2 + *(a + i *size + size - i - 1);
 
 i++;
 }
