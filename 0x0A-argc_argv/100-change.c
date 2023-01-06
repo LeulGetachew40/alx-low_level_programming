652a83083112e79ff1ec4d6c6ff09e38066d4a17
Leul Getachew <leulgetachew40@gmail.com>
2023-01-06 14:46:37 -0800

    advanced 1

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -18,7 +18,7 @@ printf("Error\n");
 return (1);
 }
 c = atoi(argv[1]);
-if(c < 0)
+if (c < 0)
 {
 printf("0\n");
 return (0);
@@ -26,29 +26,17 @@ return (0);
 for (; c >= 0;)
 {
 if (c >= 25)
-{
-c -= 25;
-}
+	c -= 25;
 else if (c >= 10)
-{
-c -= 10;
-}
+	c -= 10;
 else if (c >= 5)
-{
-c -= 5;
-}
+	c -= 5;
 else if (c >= 2)
-{
-c -= 2;
-}
+	c -= 2;
 else if (c >= 1)
-{
-c -= 1;
-}
+	c -= 1;
 else
-{
-break;
-}
+	break;
 coins += 1;
 }
 printf("%d\n", coins);
